562f7a489b804579cc8b9baf188ece3a6fca7a95
unknown <kefitouka2@gmail.com>
2024-04-05 16:21:18 +0100

    ++

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,43 @@
-
-import React from "react";
+import './Home.css'
+import React, { Fragment } from "react";
 import Header from "./Header";
 function Home() {
   return (
+    <Fragment>
   <Header />
+  <section className="numbers">
+  <div className="Container">
+    <div className="row">
+        <div className="col-md-3">
+           <h2>1278 +</h2>
+           <h6>savings </h6>
+
+
+        </div>
+        <div className="col-md-3">
+           <h2>5786+</h2>
+           <h6>photos</h6>
+
+
+        </div>
+        <div className="col-md-3">
+           <h2>1440+</h2>
+           <h6>Rockets</h6>
+
+
+        </div>
+        <div className="col-md-3">
+           <h2>7000+</h2>
+           <h6>Globs</h6>
+
+
+        </div>
+
+    </div>
+
+  </div>
+  </section>
+  </Fragment>
   );
 }
 
